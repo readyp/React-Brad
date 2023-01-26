4a944bd3009981a6bef826aeac5c7ab0212d8990
readyp <readypamungkas@gmail.com>
2023-01-27 05:05:38 +0700

    add margin bottom and margin to

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,8 +1,10 @@
 function Footer() {
     const currentYear = new Date().getFullYear();
     return (
-        <footer className='footer p-4 bg-gray-500 text-white'>
-            <h2 className="text-center mx-auto">Copyright &copy; {currentYear}</h2>
+        <footer className='footer p-4 bg-gray-500 text-white mt-4'>
+            <h2 className='text-center mx-auto'>
+                Copyright &copy; {currentYear}
+            </h2>
         </footer>
     );
 }
--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -3,11 +3,10 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
-        <nav className='navbar bg-gray-900 shadow text-white'>
+        <nav className='navbar bg-gray-900 shadow text-white mb-4'>
             {/* container */}
             <div className='container mx-auto px-4'>
                 <div className='flex flex-1 justify-between'>
-
                     {/* Navbran */}
                     <Link to={'/'} className='flex space-x-4 items-center'>
                         <FaGithub className='text-2xl' />
@@ -18,8 +17,12 @@ function Navbar() {
 
                     {/* Button */}
                     <div>
-                        <Link to={'/'} className='btn btn-ghost'>Home</Link>
-                        <Link to={'/about'} className='btn btn-ghost'>About</Link>
+                        <Link to={'/'} className='btn btn-ghost'>
+                            Home
+                        </Link>
+                        <Link to={'/about'} className='btn btn-ghost'>
+                            About
+                        </Link>
                     </div>
                 </div>
             </div>
